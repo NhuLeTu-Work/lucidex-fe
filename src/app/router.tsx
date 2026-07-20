7429589cc14f24e6063ff46ca17c8d44288fc569
Capricorn2000 <letunhu.vta@gmail.com>
2026-07-20 20:13:11 +0700

    Add Super Admin Portal

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -9,6 +9,7 @@ import { OwnerPortal } from '../pages/OwnerPortal';
 import { IssuerPortal } from '../pages/IssuerPortal';
 import { VerifierPortal } from '../pages/VerifierPortal';
 import { AdminPortal } from '../pages/AdminPortal';
+import { SuperAdminPortal } from '@/pages/SuperAdminPortal';
 
 export const router = createBrowserRouter([
   {
@@ -22,6 +23,7 @@ export const router = createBrowserRouter([
       { path: '/issuer', element: <ProtectedRoute allowedRole="issuer"><IssuerPortal /></ProtectedRoute> },
       { path: '/verifier', element: <ProtectedRoute allowedRole="verifier"><VerifierPortal /></ProtectedRoute> },
       { path: '/admin', element: <ProtectedRoute allowedRole="admin"><AdminPortal /></ProtectedRoute> },
+      { path: '/super', element: <ProtectedRoute allowedRole="super"><SuperAdminPortal /></ProtectedRoute> },
     ],
   },
 ]);
